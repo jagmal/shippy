948b05e52b7b8ed8aee25dd5ec4ffb2a04e91a2f
Jagmal Singh <jagmal@gmail.com>
2019-08-22 14:25:35 +0530

    WIP - builds and runs but user-cli is not working

--- user-cli/cli.go
+++ user-cli/cli.go
@@ -5,51 +5,50 @@ import (
 	"os"
 
 	pbUser "github.com/jagmal/shippy/user-service/proto/user"
-	microclient "github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/client"
-	"golang.org/x/net/context"
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
+	"golang.org/x/net/context"
 )
 
 func main() {
-	cmd.Init()
+//	cmd.Init()
 
 	client := pbUser.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
 
-	service := micro.newService(
+	service := micro.NewService(
 		micro.Flags(
 			cli.StringFlag{
-				Name: "name",
+				Name:  "name",
 				Usage: "Your full name",
 			},
 			cli.StringFlag{
-				Name: "email",
+				Name:  "email",
 				Usage: "Your email",
 			},
 			cli.StringFlag{
-				Name: "password",
+				Name:  "password",
 				Usage: "Your password",
 			},
 			cli.StringFlag{
-				Name: "company",
+				Name:  "company",
 				Usage: "Your company",
 			},
-		)
+		),
 	)
 
-	service.init(
+	service.Init(
 		micro.Action(func(c *cli.Context) {
 			name := c.String("name")
 			email := c.String("email")
 			password := c.String("password")
 			company := c.String("company")
 
-			r, err := client.Create(context.TODO(), &pb.User {
-				Name: name,
-				Email: email,
+			r, err := client.Create(context.TODO(), &pbUser.User{
+				Name:     name,
+				Email:    email,
 				Password: password,
-				Company: company,
+				Company:  company,
 			})
 
 			if err != nil {
@@ -58,7 +57,7 @@ func main() {
 
 			log.Printf("Created: %s", r.User.Id)
 
-			getAll, err := client.GetAll(context.Background(), &pb.Request{})
+			getAll, err := client.GetAll(context.Background(), &pbUser.Request{})
 
 			if err != nil {
 				log.Fatalf("Could not list users: %v", err)
@@ -69,7 +68,7 @@ func main() {
 			}
 
 			os.Exit(0)
-		})
+		}),
 	)
 
 	if err := service.Run(); err != nil {
